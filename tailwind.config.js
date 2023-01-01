6082c8767c0b03057fba6167103565239b889584
ahmadou5 <ahmadlasauwal@gmail.com>
2023-01-01 15:55:57 +0100

    ====

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      margin: {
+        320: "320px",
+      },
+      width: {
+        190: "190px",
+        275: "275px",
+        300: "300px",
+        340: "340px",
+        350: "350px",
+        370: "370px",
+        656: "656px",
+        880: "880px",
+        900: "900px",
+        508: "508px",
+      },
+      height: {
+        80: "80px",
+        300: "300px",
+        320: "320px",
+        340: "340px",
+        370: "370px",
+        420: "420px",
+        510: "510px",
+        600: "600px",
+        685: "685px",
+        800: "800px",
+        "90vh": "90vh",
+      },
+      colors: {
+        navBar: "#1d1e25",
+        primary: "#121316",
+        textBase: "#8f9091",
+      },
+    },
+  },
+  plugins: [],
+}
