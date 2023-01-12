ad6a86f709eb2e926b05309641c6073612e9ab0f
ahmadou5 <ahmadlasauwal@gmail.com>
2023-01-12 18:23:59 +0100

    ===2

--- src/components/Gain/Gain.js
+++ src/components/Gain/Gain.js
@@ -17,9 +17,51 @@ export const Gain = () => {
                         <div className="flex flex-col" style={{'opacity':1}}>
                             <div className="flex justify-center items-center bg-[#2190FF] xs:w-[70px] xs:h-[70px] w-[70px] h-[70px] rounded-full bg-[#2190FF]">
                                 <div className="relative rounded-full xs:w-[32px] xs:h-[32px] w-[28px] h-[28px]">
-                                    <span ></span>
+                                    <span style={{'box-sizing':'border-box'},{'display':'block'}, {'overflow':'hidden'},{'width':'initial'},{'height':'initial'},{'background':'none'},{'opacity':1},{'border':0},{'margin':0},{'padding':0},{'position':'absolute'},{'top':0},{'left':0},{'bottom':0},{'right':0}}>
+                                        <img alt="chat" src="/chat.svg" decoding="async" data-nimg="fill" style={{'position':'absolute'},{'top':0},{'left':0},{'bottom':0},{'right':0},{'box-sizing':'border-box'},{'padding':0},{'border':'none'},{'margin':'auto'},{'display':'block'},{'width':0},{'height':0},{'min-width':'100%'},{'max-width':'100%'},{'min-height':'100%'},{'max-height':'100%'}} sizes="100vw"  />
+                                    </span>
                                 </div>
                             </div>
+                            <h3 class="font-poppins font-semibold xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] dark:text-[#FFFFFF] text-pro-w-black my-5">
+                                Expert Coaching Support
+                            </h3>
+                            <p class="font-poppins xs:text-[20px] xs:leading-[27px] text-[16px] leading-[23px] font-normal dark:text-[#A3B3BC] text-[#5B6478]">
+                                Gain 24/7 access to a private community of coders looking to advance their knowledge and experience, as well as their careers.
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex-1 xs:min-w-[370px] m-2 sm:p-12 p-6 sm:rounded-[40px] rounded-[30px] dark:bg-pro-black-1 bg-pro-w-white hover:shadow-lg dark:hover:bg-pro-black-2 hover:bg-white dark:shadow-inherit shadow-pro-w-shadow2">
+                        <div className="flex flex-col" style={{'opacity':1}}>
+                            <div className="flex justify-center items-center bg-[#F16565] xs:w-[70px] xs:h-[70px] w-[70px] h-[70px] rounded-full bg-[#2190FF]">
+                                <div className="relative rounded-full xs:w-[32px] xs:h-[32px] w-[28px] h-[28px]">
+                                    <span style={{'box-sizing':'border-box'},{'display':'block'}, {'overflow':'hidden'},{'width':'initial'},{'height':'initial'},{'background':'none'},{'opacity':1},{'border':0},{'margin':0},{'padding':0},{'position':'absolute'},{'top':0},{'left':0},{'bottom':0},{'right':0}}>
+                                        <img alt="chat" src="/medal.svg" decoding="async" data-nimg="fill" style={{'position':'absolute'},{'top':0},{'left':0},{'bottom':0},{'right':0},{'box-sizing':'border-box'},{'padding':0},{'border':'none'},{'margin':'auto'},{'display':'block'},{'width':0},{'height':0},{'min-width':'100%'},{'max-width':'100%'},{'min-height':'100%'},{'max-height':'100%'}} sizes="100vw"  />
+                                    </span>
+                                </div>
+                            </div>
+                            <h3 class="font-poppins font-semibold xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] dark:text-[#FFFFFF] text-pro-w-black my-5">
+                                Real-World Projects Experience
+                            </h3>
+                            <p class="font-poppins xs:text-[20px] xs:leading-[27px] text-[16px] leading-[23px] font-normal dark:text-[#A3B3BC] text-[#5B6478]">
+                               Reading Theory will only get you so far, but building Live projects from the Ground-Up will boost your career in the software development space..
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex-1 xs:min-w-[370px] m-2 sm:p-12 p-6 sm:rounded-[40px] rounded-[30px] dark:bg-pro-black-1 bg-pro-w-white hover:shadow-lg dark:hover:bg-pro-black-2 hover:bg-white dark:shadow-inherit shadow-pro-w-shadow2">
+                        <div className="flex flex-col" style={{'opacity':1}}>
+                            <div className="flex justify-center items-center bg-[#2190FF] xs:w-[70px] xs:h-[70px] w-[70px] h-[70px] rounded-full bg-[#2190FF]">
+                                <div className="relative rounded-full xs:w-[32px] xs:h-[32px] w-[28px] h-[28px]">
+                                    <span style={{'box-sizing':'border-box'},{'display':'block'}, {'overflow':'hidden'},{'width':'initial'},{'height':'initial'},{'background':'none'},{'opacity':1},{'border':0},{'margin':0},{'padding':0},{'position':'absolute'},{'top':0},{'left':0},{'bottom':0},{'right':0}}>
+                                        <img alt="chat" src="/computer.svg" decoding="async" data-nimg="fill" style={{'position':'absolute'},{'top':0},{'left':0},{'bottom':0},{'right':0},{'box-sizing':'border-box'},{'padding':0},{'border':'none'},{'margin':'auto'},{'display':'block'},{'width':0},{'height':0},{'min-width':'100%'},{'max-width':'100%'},{'min-height':'100%'},{'max-height':'100%'}} sizes="100vw"  />
+                                    </span>
+                                </div>
+                            </div>
+                            <h3 class="font-poppins font-semibold xs:text-[24px] xs:leading-[30px] text-[20px] leading-[26px] tracking-[0.25%] dark:text-[#FFFFFF] text-pro-w-black my-5">
+                                AT Academy Certificte
+                            </h3>
+                            <p class="font-poppins xs:text-[20px] xs:leading-[27px] text-[16px] leading-[23px] font-normal dark:text-[#A3B3BC] text-[#5B6478]">
+                              Earn AT Academy certification for each course you complete and show hiring managers and clients that you are a pro-Grammer.
+                            </p>
                         </div>
                     </div>
                 </div>
